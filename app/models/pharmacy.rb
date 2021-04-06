f98606dce5e4f282b0ddd164c6d2c45a950cd3a6
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 10:38:22 -0400

    Pharmacy.rb had nested attributes added.

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -2,6 +2,8 @@ class Pharmacy < ApplicationRecord
     belongs_to :medication
     belongs_to :patient
     has_one :manager
+    accepts_nested_attributes_for :ingredients
+    accepts_nested_attributes_for :managers
 
     validates :name, presence: true
 end
