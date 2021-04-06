697f4cc68de543498aa8cad8e3ba8e62a2d961f1
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 09:21:20 -0400

    routes.rb was modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :managers
-  resources :patients
   resources :pharmacies
-  resources :medications
+   
+  resources :medications do
+    resources :patients, only: [:index, :show]
+  end
+  resources :patients
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
