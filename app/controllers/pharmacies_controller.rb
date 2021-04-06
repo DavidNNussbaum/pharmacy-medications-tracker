d58a9219cd95bf79675f5c630b50443025d14820
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 09:07:57 -0400

    pharmacies_controller was started.

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,2 +1,56 @@
 class PharmaciesController < ApplicationController
+    before_action :find_pharmacy, only: [:show, :edit, :update, :destroy]
+
+     def index
+       @pharmacies = Pharmacy.all.order('name')
+     end
+
+    def show
+         
+    end
+  
+    def new
+      @pharmacy = Pharmacy.new
+   
+    end
+  
+    def edit
+ 
+    end
+  
+    def create
+      @pharmacy = Pharmacy.new(pharmacy_params)
+      if @pharmacy.save
+            redirect_to @pharmacy 
+        else  
+            render :new
+        end
+    end
+  
+    def update
+      if @pharmacy.update(pharmacy_params)
+        redirect_to pharmacy_path(@pharmacy)
+      else 
+        render :edit
+      end
+      
+    end
+
+
+  def destroy
+        if @pharmacy.destroy
+            redirect_to pharmacy_path
+        else
+            redirect_to back
+        end
+  end
+private
+
+  def find_pharmacy
+    @pharmacy = Pharmacy.find(params[:id])
+  end
+  
+    def pharmacy_params
+      params.require(:pharmacy).permit!
+    end
 end
