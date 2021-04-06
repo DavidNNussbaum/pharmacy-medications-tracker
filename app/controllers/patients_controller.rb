0d81bf003bd5f1d8d0aa1196ed2b6a0174470c14
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 09:02:08 -0400

    patients_controller was started.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,56 @@
 class PatientsController < ApplicationController
+    before_action :find_patient, only: [:show, :edit, :update, :destroy]
+
+     def index
+       @patients = Patient.all.order('name')
+     end
+
+    def show
+         
+    end
+  
+    def new
+      @patient = Patient.new
+   
+    end
+  
+    def edit
+ 
+    end
+  
+    def create
+      @patient = Patient.new(patient_params)
+      if @patient.save
+            redirect_to @patient 
+        else  
+            render :new
+        end
+    end
+  
+    def update
+      if @patient.update(patient_params)
+        redirect_to patienty_path(@patient)
+      else 
+        render :edit
+      end
+      
+    end
+
+
+  def destroy
+        if @patient.destroy
+            redirect_to patient_path
+        else
+            redirect_to back
+        end
+  end
+private
+
+  def find_patient
+    @patient = Patient.find(params[:id])
+  end
+  
+    def patient_params
+      params.require(:patient).permit!
+    end
 end
