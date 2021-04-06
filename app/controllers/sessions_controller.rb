8a8f1916dc0a8197e29bd44347c1a24c20419e72
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 14:42:41 -0400

    before_action :redirect_if_not_logged_in was added to controllers.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+    before_action :redirect_if_logged_in, only: [:new]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      user = User.find_by_email(params[:user][:email])
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to user
+      else
+        redirect_to '/login', error: "Invalid credentials"
+      end
+    end
+  
+    def omniauth
+      @user = User.from_omniauth(auth)
+      if @user.valid?
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      session.clear
+      redirect_to "/login"
+    end
+  
+    private
+  
+    def auth
+      request.env['omniauth.auth']
+    end
+  end
