f1f9a4269f05683fbae090eff1e09d203056c72f
DavidNNussbaum <brocknynj@aol.com>
2021-04-06 13:49:58 -0400

    Views as well as application_helper were modified.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def object_errors(object)
+        if object.errors.any?
+         render partial: 'pharmacies/errors', locals: {object: object}
+        end
+     end
 end
